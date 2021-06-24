ea80e13d93e407f502b6dcc189c73cafb9e33048
aprilahijriyan <hijriyan23@gmail.com>
2021-06-25 01:35:39 +0700

    🐛 FIX: pre-commit tool

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,12 @@
 import pathlib
 
+cwd = pathlib.Path.cwd()
 {% if cookiecutter.database_engine == 'sqlalchemy' -%}
-(pathlib.Path.cwd() / "extensions/mongo.py").unlink()
+(cwd / "extensions/mongo.py").unlink()
 {% elif cookiecutter.database_engine == 'mongoengine' -%}
-(pathlib.Path.cwd() / "extensions/sqla.py").unlink()
+(cwd / "extensions/sqla.py").unlink()
 {% endif %}
 
 {% if cookiecutter.pre_commit|lower != 'y' -%}
-(pathlib.Path.cwd() / ".pre-commit-config.yml").unlink()
+(cwd / ".pre-commit-config.yml").unlink()
 {% endif %}
